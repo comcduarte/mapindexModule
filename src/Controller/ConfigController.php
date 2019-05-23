4999491547c9ff7f7435e44fda37771b6246f340
comcduarte <33262448+comcduarte@users.noreply.github.com>
2019-05-23 16:04:02 -0400

    Cleared unused code

--- src/Controller/ConfigController.php
+++ src/Controller/ConfigController.php
@@ -1,12 +1,10 @@
 <?php 
 namespace Mapindex\Controller;
 
-use Annotation\Model\AnnotationModel;
 use Mapindex\Form\UploadFileForm;
 use Mapindex\Model\MapindexModel;
 use Mapindex\Model\OwnerModel;
 use Midnet\Model\Uuid;
-use PHPUnit\Framework\Constraint\FileExists;
 use Zend\Db\Adapter\AdapterAwareTrait;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Validator\Db\RecordExists;
@@ -43,11 +41,6 @@ class ConfigController extends AbstractActionController
         
         $this->flashMessenger()->addSuccessMessage('Database tables cleared.');
         return $this->redirect()->toRoute('maps/config');
-    }
-    
-    public function createDatabaseTable()
-    {
-        $sql = "CREATE TABLE `map-index_dev`.`maps` ( `UUID` VARCHAR(36) NOT NULL , `STATUS` INT NULL , `DATE_CREATED` TIMESTAMP NULL , `DATE_MODIFIED` TIMESTAMP NULL , PRIMARY KEY (`UUID`(36))) ENGINE = InnoDB;";
     }
     
     public function clearDatabaseTables()
@@ -87,11 +80,8 @@ class ConfigController extends AbstractActionController
                 $uuid = new Uuid();
                 $date = new \DateTime('now',new \DateTimeZone('EDT'));
                 $today = $date->format('Y-m-d H:i:s');
-                $year = $date->format('Y');
-                $annotation = new AnnotationModel($this->adapter);
                 $index = new MapindexModel($this->adapter);
                 $owner = new OwnerModel($this->adapter);
-                $previous_uuid = null;
                 
                 //-- Database Validators --//
                 $validator = new RecordExists([
@@ -102,8 +92,6 @@ class ConfigController extends AbstractActionController
                 $validator->setAdapter($this->adapter);
                 $validator->setField('NAME');
                 $validator->setTable($owner->getTableName());
-                
-                $file_validator = new FileExists();
                 
                 //-- Define CSV Position variables --//
                 $MAP = 0;
@@ -118,7 +106,6 @@ class ConfigController extends AbstractActionController
                 $row = 0;
                 if (($handle = fopen($data['FILE']['tmp_name'],"r")) !== FALSE) {
                     //-- Read record from file --//
-//                     while (($record = fgetcsv($handle, 1000, ",")) !== FALSE) {
                     while ($s = fgets($handle, 1024)) {
                         $record = [];
                         /****************************************
@@ -197,14 +184,9 @@ class ConfigController extends AbstractActionController
                         $filed = \DateTime::createFromFormat('d-M-Y', $record[$DATE_FILED]);
                         $index->DATE_FILED = date_format($filed, 'Y-m-d');
                         
-//                         $index->DATE_DRAWN = date('Y-m-d H:i:s',strtotime($record[$DATE_DRAWN]));
-//                         $index->DATE_FILED = date('Y-m-d H:i:s',strtotime($record[$DATE_FILED]));
-                        
                         $index->UUID = $uuid->generate()->value;
                         $index->DATE_CREATED = $today;
                         $index->STATUS = $index::ACTIVE_STATUS;
-                        
-                        $previous_uuid = $index->UUID;
                         
                         $index->create();
                         
